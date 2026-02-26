dc12008e88f2b3dd8bc63868b6f4a83483bc948a
Matthias Tams <tams@ahp-gmbh.de>
2026-02-26 15:07:59 +0100

    WIP: SessionManager Erstversion.

--- tools/OcctRestServer/RestServer.cxx
+++ tools/OcctRestServer/RestServer.cxx
@@ -1,20 +1,24 @@
 #include "RestServer.hxx"
 #include "Endpoints.hxx"
+#include "SessionManager.hpp"
 
 Standard_Integer StartOcctRestServer(Standard_Integer thePort)
 {
-    httplib::Server aServer;
+    httplib::Server svr;
+  SessionManager  manager;
 
     // Endpoints registrieren
-    RegisterBBoxEndpoint(aServer);
+    RegisterBBoxEndpoint(svr);
     // Weitere Endpoints hier hinzufügen:
     // RegisterMeshEndpoint(aServer);
     // RegisterStructureEndpoint(aServer);
+    RegisterQuitEndpoint(svr);
+    RegisterSessionManagerEndpoints(svr, manager);
 
     std::cout << "OCCT REST server listening on port " << thePort << std::endl;
 
     // Blockiert bis Server beendet wird
-    aServer.listen("0.0.0.0", thePort);
+    svr.listen("0.0.0.0", thePort);
 
     return 0;
 }
