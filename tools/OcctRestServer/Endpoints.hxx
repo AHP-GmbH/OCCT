d65f709d11ecd2af3fcbe6a5aefc53e49fff35c4
Matthias Tams <tams@ahp-gmbh.de>
2026-02-26 15:09:13 +0100

    Revert "WIP: SessionManager Erstversion."
    This reverts commit dc12008e88f2b3dd8bc63868b6f4a83483bc948a.

--- tools/OcctRestServer/Endpoints.hxx
+++ tools/OcctRestServer/Endpoints.hxx
@@ -7,9 +7,6 @@
 
 #include "httplib.h"
 #include "json.hpp"
-#include "SessionManager.hpp"
 
 // Registriert den /bbox Endpoint
-void RegisterBBoxEndpoint(httplib::Server& svr);
-void RegisterQuitEndpoint(httplib::Server& svr);
-void RegisterSessionManagerEndpoints(httplib::Server& svr, SessionManager& manager);
+void RegisterBBoxEndpoint(httplib::Server& theServer);
--- tools/OcctRestServer/RestServer.cxx
+++ tools/OcctRestServer/RestServer.cxx
@@ -1,24 +1,20 @@
 #include "RestServer.hxx"
 #include "Endpoints.hxx"
-#include "SessionManager.hpp"
 
 Standard_Integer StartOcctRestServer(Standard_Integer thePort)
 {
-    httplib::Server svr;
-  SessionManager  manager;
+    httplib::Server aServer;
 
     // Endpoints registrieren
-    RegisterBBoxEndpoint(svr);
+    RegisterBBoxEndpoint(aServer);
     // Weitere Endpoints hier hinzufügen:
     // RegisterMeshEndpoint(aServer);
     // RegisterStructureEndpoint(aServer);
-    RegisterQuitEndpoint(svr);
-    RegisterSessionManagerEndpoints(svr, manager);
 
     std::cout << "OCCT REST server listening on port " << thePort << std::endl;
 
     // Blockiert bis Server beendet wird
-    svr.listen("0.0.0.0", thePort);
+    aServer.listen("0.0.0.0", thePort);
 
     return 0;
 }
